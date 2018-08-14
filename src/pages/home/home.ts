2effcbc6e01058906abd1aa83f19e7117b83cc16
Bren <brenbln94@gmail.com>
2018-08-14 16:49:02 -0700

    Fixed a bug where images weren't moving correctly

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,8 +34,74 @@ export class HomePage {
   onLongPress(event, button) {
 
     this.multiSelectEnabled = true;
-    if (!this.alertControlEnabled) {
-      console.log(button.name + button.type);
+    // if (!this.alertControlEnabled) {
+    //   console.log(button.name + button.type);
+    //   let alert = this.alertCtrl.create();
+    //   this.alertControlEnabled = true;
+    //   alert.setTitle("Change type");
+    //   alert.addInput({
+    //     type: 'radio',
+    //     label: 'Alpha',
+    //     value: 'Alpha',
+    //   });
+
+    //   alert.addInput({
+    //     type: 'radio',
+    //     label: 'Beta',
+    //     value: 'Beta',
+    //   });
+
+    //   alert.addButton({
+    //     text: 'Cancel',
+    //     handler: () => {
+    //       this.alertControlEnabled = false;
+    //     }
+    //   });
+
+    //   alert.addButton({
+    //     text: 'Move',
+    //     handler: (data: any) => {
+    //       console.log('Checkbox data:', data);
+    //       this.testCheckboxOpen = false;
+    //       this.testCheckboxResult = data;
+    //       button.setType(data.toString());
+    //       this.alertControlEnabled = false;
+    //       console.log('Button changed to: ' + button.type.toString());
+    //     }
+    //   });
+
+    //   alert.present();
+    // }
+  }
+
+  updateList(button) {
+
+    if (!button.multiListCheck) {
+      button.multiListCheck = true;
+      console.log("DEBUG|LOG: Checked " + button.name);
+    } else {
+      button.multiListCheck = false;
+      console.log("DEBUG|LOG: Unchecked " + button.name);
+    }
+
+  }
+
+  onPressRelease(event, button) {
+    this.alertControlEnabled = false;
+  }
+
+  onPress(event, button) {
+
+  }
+
+  prev() {
+    // Reset all settings and uncheck all images
+    this.multiSelectEnabled = false;
+    this.resetMultiListCheck();
+  }
+
+  onMovePress(event) {
+    
       let alert = this.alertCtrl.create();
       this.alertControlEnabled = true;
       alert.setTitle("Change type");
@@ -64,32 +130,31 @@ export class HomePage {
           console.log('Checkbox data:', data);
           this.testCheckboxOpen = false;
           this.testCheckboxResult = data;
-          button.setType(data.toString());
+          this.buttons.forEach(element => {
+            if (element.multiListCheck) {
+              element.setType(data.toString());
+            }
+          });
           this.alertControlEnabled = false;
-          console.log('Button changed to: ' + button.type.toString());
+          this.resetMultiListCheck();
         }
       });
 
       alert.present();
     }
-  }
-
-  onPressRelease(event, button) {
-    this.alertControlEnabled = false;
-  }
-
-  onPress(event, button) {
-
-  }
-
-  prev() {
-    this.multiSelectEnabled = false;
-  }
+  
+    // Helper functions
+    resetMultiListCheck() {
+      this.buttons.forEach(element => {
+        element.multiListCheck = false;
+      });
+    }
 
 }
 
 class Button {
   img: string;
+  multiListCheck: boolean = false;
   constructor(public type: string, public name: string) {
     this.type = type;
     this.name = name;
